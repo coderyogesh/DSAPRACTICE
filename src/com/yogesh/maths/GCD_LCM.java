578d68f66d1ddd43002775b046fd22aed2b4494b
Yogesh <yogeshjangid27@icloud.com>
2022-03-14 17:44:32 +0530

    All the work done by me in previous 5 days.

---
+++ src/com/yogesh/maths/GCD_LCM.java
@@ -0,0 +1,17 @@
+package com.yogesh.maths;
+
+public class GCD_LCM {
+    public static void main(String[] args) {
+        System.out.println(lcm(9,18));
+    }
+    static int gcd(int a, int b){
+        if (a == 0) {
+            return b;
+        }
+        return gcd(b%a,a);
+    } 
+
+    static int lcm(int a, int b) {
+        return a*b/gcd(a,b);
+    }
+}
